713bf0921ffdad5d248f5ad4ff530e997f38cc02
Daniel <DanielSebastianrf@gmail.com>
2025-05-30 10:54:31 -0400

    adding nodemon and npm run script

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-const http = require("node:http");
+const http = require('node:http')
 
 const server = http.createServer((req, res) => {
-    console.log("Enviando contenido")
-    console.log(req.url)
+  console.log('Enviando contenido')
+  console.log(req.url)
 
-    if(req.url === "/show-html") {
-        res.setHeader('content-type','text/html; charset=utf-8')
-        res.statusCode =  "200"
-        res.end("<h1>Hola Mundo página con node.js</h1>")
-    }
-
-    
+  if (req.url === '/show-html') {
+    res.setHeader('content-type', 'text/html; charset=utf-8')
+    res.statusCode = '200'
+    res.end('<h1>Hola Mundo página con node.js</h1>')
+  }
 })
 
-
 server.listen(1234, () => {
-    console.log("Escuchando servidor en el puerto: http://localhost:"+server.address().port)
+  console.log('Escuchando servidor en el puerto: http://localhost:' + server.address().port)
 })
